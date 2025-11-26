58ddbce499521640bf37193fb0ad66d016224054
gaelmiau <gaelmartinez050@gmail.com>
2025-11-26 08:57:07 -0600

    Correccion de POJOs (Envio, Historial, Paquete)

--- src/java/pojo/Envio.java
+++ src/java/pojo/Envio.java
@@ -1,11 +1,13 @@
 package pojo;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @authors Ohana & Benito
  */
 public class Envio {
-    
+
     private Integer idEnvio;
     private String numeroGuia;
     private String fechaEnvio;
@@ -13,6 +15,7 @@ public class Envio {
     private String nombreDestinatario;
     private String apellidoPaternoDestinatario;
     private String apellidoMaternoDestinatario;
+    private BigDecimal costo;
     // Foreign Keys
     private Integer idClienteRemitente;
     private Integer idSucursalOrigen;
@@ -21,7 +24,145 @@ public class Envio {
     private Integer idUnidad;
     private Integer idDireccionDestino;
     private Integer idEstatus;
-    
-    // falta investigar nuevo atributo COSTO y getters y setters
-    
+
+    public Envio() {
+    }
+
+    public Envio(Integer idEnvio, String numeroGuia, String fechaEnvio, String fechaEntrega, String nombreDestinatario, String apellidoPaternoDestinatario, String apellidoMaternoDestinatario, BigDecimal costo, Integer idClienteRemitente, Integer idSucursalOrigen, Integer idSucursalDestino, Integer idColaborador, Integer idUnidad, Integer idDireccionDestino, Integer idEstatus) {
+        this.idEnvio = idEnvio;
+        this.numeroGuia = numeroGuia;
+        this.fechaEnvio = fechaEnvio;
+        this.fechaEntrega = fechaEntrega;
+        this.nombreDestinatario = nombreDestinatario;
+        this.apellidoPaternoDestinatario = apellidoPaternoDestinatario;
+        this.apellidoMaternoDestinatario = apellidoMaternoDestinatario;
+        this.costo = costo;
+        this.idClienteRemitente = idClienteRemitente;
+        this.idSucursalOrigen = idSucursalOrigen;
+        this.idSucursalDestino = idSucursalDestino;
+        this.idColaborador = idColaborador;
+        this.idUnidad = idUnidad;
+        this.idDireccionDestino = idDireccionDestino;
+        this.idEstatus = idEstatus;
+    }
+
+    public void setIdEnvio(Integer idEnvio) {
+        this.idEnvio = idEnvio;
+    }
+
+    public void setNumeroGuia(String numeroGuia) {
+        this.numeroGuia = numeroGuia;
+    }
+
+    public void setFechaEnvio(String fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public void setNombreDestinatario(String nombreDestinatario) {
+        this.nombreDestinatario = nombreDestinatario;
+    }
+
+    public void setApellidoPaternoDestinatario(String apellidoPaternoDestinatario) {
+        this.apellidoPaternoDestinatario = apellidoPaternoDestinatario;
+    }
+
+    public void setApellidoMaternoDestinatario(String apellidoMaternoDestinatario) {
+        this.apellidoMaternoDestinatario = apellidoMaternoDestinatario;
+    }
+
+    public void setCosto(BigDecimal costo) {
+        this.costo = costo;
+    }
+
+    public void setIdClienteRemitente(Integer idClienteRemitente) {
+        this.idClienteRemitente = idClienteRemitente;
+    }
+
+    public void setIdSucursalOrigen(Integer idSucursalOrigen) {
+        this.idSucursalOrigen = idSucursalOrigen;
+    }
+
+    public void setIdSucursalDestino(Integer idSucursalDestino) {
+        this.idSucursalDestino = idSucursalDestino;
+    }
+
+    public void setIdColaborador(Integer idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+
+    public void setIdUnidad(Integer idUnidad) {
+        this.idUnidad = idUnidad;
+    }
+
+    public void setIdDireccionDestino(Integer idDireccionDestino) {
+        this.idDireccionDestino = idDireccionDestino;
+    }
+
+    public void setIdEstatus(Integer idEstatus) {
+        this.idEstatus = idEstatus;
+    }
+
+    public Integer getIdEnvio() {
+        return idEnvio;
+    }
+
+    public String getNumeroGuia() {
+        return numeroGuia;
+    }
+
+    public String getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public String getNombreDestinatario() {
+        return nombreDestinatario;
+    }
+
+    public String getApellidoPaternoDestinatario() {
+        return apellidoPaternoDestinatario;
+    }
+
+    public String getApellidoMaternoDestinatario() {
+        return apellidoMaternoDestinatario;
+    }
+
+    public BigDecimal getCosto() {
+        return costo;
+    }
+
+    public Integer getIdClienteRemitente() {
+        return idClienteRemitente;
+    }
+
+    public Integer getIdSucursalOrigen() {
+        return idSucursalOrigen;
+    }
+
+    public Integer getIdSucursalDestino() {
+        return idSucursalDestino;
+    }
+
+    public Integer getIdColaborador() {
+        return idColaborador;
+    }
+
+    public Integer getIdUnidad() {
+        return idUnidad;
+    }
+
+    public Integer getIdDireccionDestino() {
+        return idDireccionDestino;
+    }
+
+    public Integer getIdEstatus() {
+        return idEstatus;
+    }
 }
--- src/java/pojo/EstatusEnvioHistorial.java
+++ src/java/pojo/EstatusEnvioHistorial.java
@@ -7,15 +7,72 @@ import java.sql.Timestamp;
  * @authors Ohana & Benito
  */
 public class EstatusEnvioHistorial {
-    
+
     private Integer idHistorial;
     private String comentario;
-    private Timestamp fechaHora; 
+    private String fechaHora;
     // Foreign Keys
     private Integer idEnvio;
     private Integer idEstatus;
-    private Integer idColaborador; 
-    
-    // falta getters y setters, posible cambio de tipo fechaHora
-    
+    private Integer idColaborador;
+
+    public EstatusEnvioHistorial() {
+    }
+
+    public EstatusEnvioHistorial(Integer idHistorial, String comentario, String fechaHora, Integer idEnvio, Integer idEstatus, Integer idColaborador) {
+        this.idHistorial = idHistorial;
+        this.comentario = comentario;
+        this.fechaHora = fechaHora;
+        this.idEnvio = idEnvio;
+        this.idEstatus = idEstatus;
+        this.idColaborador = idColaborador;
+    }
+
+    public void setIdHistorial(Integer idHistorial) {
+        this.idHistorial = idHistorial;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public void setIdEnvio(Integer idEnvio) {
+        this.idEnvio = idEnvio;
+    }
+
+    public void setIdEstatus(Integer idEstatus) {
+        this.idEstatus = idEstatus;
+    }
+
+    public void setIdColaborador(Integer idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+
+    public Integer getIdHistorial() {
+        return idHistorial;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public Integer getIdEnvio() {
+        return idEnvio;
+    }
+
+    public Integer getIdEstatus() {
+        return idEstatus;
+    }
+
+    public Integer getIdColaborador() {
+        return idColaborador;
+    }
 }
