58168772174cc290da0fbf4d1372845780c15181
OhanaMGH <ohanagutierrez229@gmail.com>
2025-11-27 16:23:59 -0600

    Agregar endpoint de autenticación de colaborador con FormParam

--- src/java/ws/ColaboradorWS.java
+++ src/java/ws/ColaboradorWS.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ws;
 
-/**
- *
- * @author benit
- */
+import dominio.ColaboradorImp;
+import dto.RSColaborador;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("autenticacion")
 public class ColaboradorWS {
+    private RSColaborador autenticarColaborador(){
+        return null;
+    }
     
+    @Path("administracion/colaborador")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public RSColaborador autenticacionColaborador(
+        @FormParam("noPersonal") String  noPersonal,
+        @FormParam("password") String  password ){
+        if(noPersonal!= null && !noPersonal.isEmpty()&&
+                (password != null && !password.isEmpty())){
+            return ColaboradorImp.autenticarAdministracion(noPersonal, password);
+        }
+        throw new BadRequestException();
+    }
 }
