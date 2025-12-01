2fe6afe4821917560299ada03167baec84c01111
OhanaMGH <ohanagutierrez229@gmail.com>
2025-11-30 18:36:53 -0600

    Implementar método obtenerEnviosPorConductor en EnvioWS

---
+++ src/java/ws/EnvioWS.java
@@ -0,0 +1,28 @@
+package ws;
+
+import dominio.EnvioImp;
+import dto.RSEnvioLista;
+import java.util.List;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("envio") 
+public class EnvioWS {
+
+    @GET
+    @Path("porConductor/{idColaborador}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<RSEnvioLista> obtenerEnviosPorConductor(
+            @PathParam("idColaborador") Integer idColaborador) {
+
+        if (idColaborador == null || idColaborador <= 0) {
+            throw new BadRequestException("ID conductor inválido");
+        }
+
+        return EnvioImp.obtenerEnviosPorConductor(idColaborador);
+    }
+}
