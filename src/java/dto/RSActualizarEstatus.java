2afccb3b6ab707f67a96b805e7aee8229cd43900
OhanaMGH <ohanagutierrez229@gmail.com>
2025-12-10 08:11:26 -0600

    Se agrega RS para la actualización del estatus con (numeroGuia, nuevoId, comentario, idColaborador)

---
+++ src/java/dto/RSActualizarEstatus.java
@@ -0,0 +1,57 @@
+
+package dto;
+
+/**
+ *
+ * @author ohana & benito
+ */
+public class RSActualizarEstatus {
+    private String numeroGuia;
+    private int nuevoIdEstatus;
+    private String comentario;
+    private int idColaborador; //conductor que realiza la acción
+
+    public RSActualizarEstatus(String numeroGuia, int nuevoIdEstatus, String comentario, int idColaborador) {
+        this.numeroGuia = numeroGuia;
+        this.nuevoIdEstatus = nuevoIdEstatus;
+        this.comentario = comentario;
+        this.idColaborador = idColaborador;
+    }
+
+    public RSActualizarEstatus() {
+    }
+
+    public String getNumeroGuia() {
+        return numeroGuia;
+    }
+
+    public int getNuevoIdEstatus() {
+        return nuevoIdEstatus;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public int getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setNumeroGuia(String numeroGuia) {
+        this.numeroGuia = numeroGuia;
+    }
+
+    public void setNuevoIdEstatus(int nuevoIdEstatus) {
+        this.nuevoIdEstatus = nuevoIdEstatus;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setIdColaborador(int idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+    
+    
+}
